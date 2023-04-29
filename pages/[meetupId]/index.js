3cad8bee1dc2183ade16236d660f86e4af8f4209
justicepelteir <justice.douglas@hey.com>
2023-04-28 20:27:51 -0700

    add meetupdetail component

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://www.meetup.com/blog/wp-content/uploads/2020/08/holding-hands.jpg"
+      title="First Meetup"
+      address="Test street 12345"
+      description="test description"
+    />
+  );
+}
+
+export default MeetupDetails;
