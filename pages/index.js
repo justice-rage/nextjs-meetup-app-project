d56208e5a407b2e65ef38f1faffd3ef47ec99498
justicepelteir <justice.douglas@hey.com>
2023-04-30 11:26:19 -0700

    set up data fetching for static pages

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,43 @@
 import MeetupList from "../components/meetups/MeetupList";
 
 const DUMMY_MEETUPS = [
-    {
-        id: 'm1',
-        title: 'A First Meetup',
-        image: 'https://www.meetup.com/blog/wp-content/uploads/2020/08/holding-hands.jpg',
-        address: 'Test address 12345 SE St',
-        description: 'This is a first meetup.'
-    },
-    {
-        id: 'm2',
-        title: 'A Second Meetup',
-        image: 'https://www.meetup.com/blog/wp-content/uploads/2020/08/holding-hands.jpg',
-        address: 'Test address 12345 SE St',
-        description: 'This is a first meetup.'
-    },
-    {
-        id: 'm3',
-        title: 'A Third Meetup',
-        image: 'https://www.meetup.com/blog/wp-content/uploads/2020/08/holding-hands.jpg',
-        address: 'Test address 12345 SE St',
-        description: 'This is a first meetup.'
-    },
+  {
+    id: "m1",
+    title: "A First Meetup",
+    image:
+      "https://www.meetup.com/blog/wp-content/uploads/2020/08/holding-hands.jpg",
+    address: "Test address 12345 SE St",
+    description: "This is a first meetup.",
+  },
+  {
+    id: "m2",
+    title: "A Second Meetup",
+    image:
+      "https://www.meetup.com/blog/wp-content/uploads/2020/08/holding-hands.jpg",
+    address: "Test address 12345 SE St",
+    description: "This is a first meetup.",
+  },
+  {
+    id: "m3",
+    title: "A Third Meetup",
+    image:
+      "https://www.meetup.com/blog/wp-content/uploads/2020/08/holding-hands.jpg",
+    address: "Test address 12345 SE St",
+    description: "This is a first meetup.",
+  },
 ];
 
-function HomePage() {
-    return <MeetupList meetups={DUMMY_MEETUPS} />
+function HomePage(props) {
+  return <MeetupList meetups={props.meetups} />;
+}
+
+export async function getStaticProps() {
+  // fetch data from an API
+  return {
+    props: {
+      meetups: DUMMY_MEETUPS,
+    },
+  };
 }
 
 export default HomePage;
