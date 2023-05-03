da1221cba682becba30f16f5b78ca9818cc65b81
justicepelteir <justice.douglas@hey.com>
2023-05-03 15:25:21 -0700

    add params for data fetching

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -11,4 +11,25 @@ function MeetupDetails() {
   );
 }
 
+export function getStaticProps(context) {
+  // fetch data for a single meetup
+
+  const meetupId = context.params.meetupId;
+
+  console.log(meetupId);
+
+  return {
+    props: {
+      meetupData: {
+        image:
+          "https://www.meetup.com/blog/wp-content/uploads/2020/08/holding-hands.jpg",
+        id: meetupId,
+        title: "My First Meetup",
+        address: "Test street 12345",
+        description: "test description",
+      },
+    },
+  };
+}
+
 export default MeetupDetails;
