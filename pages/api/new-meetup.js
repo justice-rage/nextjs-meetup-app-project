0473373705f4abd955cf833edef8407b6733fb2c
justicepelteir <justice.douglas@hey.com>
2023-05-03 18:34:13 -0700

    send http requests to api routes

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -4,22 +4,24 @@ import { MongoClient } from "mongodb";
 // POST /api/new-meetup
 
 async function handler(req, res) {
-    if (req.method === 'POST') {
-        const data = req.body;
+  if (req.method === "POST") {
+    const data = req.body;
 
-        const client = await MongoClient.connect('mongodb+srv://justice:password123abc@cluster0.ag6xbnz.mongodb.net/?retryWrites=true&w=majority');
-        const db = client.db();
+    const client = await MongoClient.connect(
+      "mongodb+srv://justice:password123abc@cluster0.ag6xbnz.mongodb.net/?retryWrites=true&w=majority"
+    );
+    const db = client.db();
 
-        const meetupsCollection = db.collection('cluster0');
+    const meetupsCollection = db.collection("cluster0");
 
-        const result = await meetupsCollection.insertOne(data);
+    const result = await meetupsCollection.insertOne(data);
 
-        console.log(result);
+    console.log(result);
 
-        client.close();
+    client.close();
 
-        res.status(201).json({ message: 'Meetup inserted!' });
-    }
+    res.status(201).json({ message: "Meetup inserted!" });
+  }
 }
 
 export default handler;
