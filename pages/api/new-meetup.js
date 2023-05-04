1c2ff60c74150d2af5504a9f6cc0768dcdd2395c
justicepelteir <justice.douglas@hey.com>
2023-05-03 17:07:03 -0700

    connect mongodb

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,11 +1,24 @@
+import { MongoClient } from "mongodb";
+
 // /api/new-meetup
 // POST /api/new-meetup
 
-function handler(req, res) {
+async function handler(req, res) {
     if (req.method === 'POST') {
         const data = req.body;
 
-        const { title, image, address, description } = data;
+        const client = await MongoClient.connect('mongodb+srv://justice:password123abc@cluster0.ag6xbnz.mongodb.net/?retryWrites=true&w=majority');
+        const db = client.db();
+
+        const meetupsCollection = db.collection('cluster0');
+
+        const result = await meetupsCollection.insertOne(data);
+
+        console.log(result);
+
+        client.close();
+
+        res.status(201).json({ message: 'Meetup inserted!' });
     }
 }
 
